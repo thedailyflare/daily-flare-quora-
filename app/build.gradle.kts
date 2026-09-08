76296e37d4b1b5e8b1b25d80e805deeb63b65bd8
The daily flare <121073238+thedailyflare@users.noreply.github.com>
2026-09-09 00:48:05 +0200

    Initial Daily Flare Quora Android app

---
+++ app/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+
+android { namespace = "com.thedailyflare.quora"; compileSdk = 35
+ defaultConfig { applicationId = "com.thedailyflare.quora"; minSdk = 24; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+}
